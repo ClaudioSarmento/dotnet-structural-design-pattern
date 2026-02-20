3923ca4f97463124809bd2a190c3bfe37e5ebc31
ClaudioSarmento <claudio-s-i@hotmail.com>
2026-02-19 21:17:05 -0300

    Composite - Compor objetos em estruturas de árvore para representar hierarquia partes-todo. Composite permite aos clientes tratarem de maneira uniforme objetos individuais e composições de objetos.

---
+++ Composite/FuncionarioLeaf.cs
@@ -0,0 +1,13 @@
+﻿namespace Composite;
+
+public class FuncionarioLeaf : Funcionario
+{
+    public FuncionarioLeaf(string nome) : base(nome)
+    {
+    }
+
+    public override string Operation()
+    {
+        return "Leaf";
+    }
+}
